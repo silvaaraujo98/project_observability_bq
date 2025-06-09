31d9ac5f84c4dd98bbacc2770572526828c7deb5
silvaaraujo98 <jvsilvaaraujo98@gmail.com>
2025-06-09 14:51:24 -0300

    feat: creating a function to get value columns in the last x hours

--- src/aux_functions.py
+++ src/aux_functions.py
@@ -1,3 +1,4 @@
+import datetime
 def get_specific_columns(df,*columns_to_select):
 
   return df[list(columns_to_select)]
@@ -7,3 +8,13 @@ def group_and_aggregate_data(df, numerical_column, *categorical_columns, aggrega
   grouped_df = df.groupby(list(categorical_columns))[numerical_column].agg(aggregation_method).reset_index()
   
   return grouped_df
+
+
+def get_value_columns_in_hours(df,date_column,value_column,hours=24,agg_func='sum'):
+  max_column_date = max(df[date_column])
+  df_filtered = df[(df[date_column]<=max_column_date) & (df[date_column] > max_column_date - datetime.timedelta(hours=hours))]
+  agg_func_value_column = df_filtered[value_column].agg(agg_func)
+  return agg_func_value_column
+
+## test
+
